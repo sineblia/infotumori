8d916ef0f201c0ea65a8084c6441aa5c05ee59f5
simoneb1x <moneb1x@gmail.com>
2021-06-04 14:54:37 +0200

    deleted all unnecessary files, updated npm, resolved bugs in deployment

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,7 +2,6 @@ import { Modal } from '@redq/reuse-modal';
 import '@redq/reuse-modal/es/index.css';
 import 'common/assets/css/flaticon.css';
 import 'swiper/swiper-bundle.css';
-import '../containers/CryptoModern/CountDown/timer.css';
 import 'common/assets/css/icon-example-page.css';
 import 'tailwindcss/tailwind.css';
 
