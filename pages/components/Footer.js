96cef662f1aaf20f5bb32a2cb4d38f1103deda84
simoneb1x <moneb1x@gmail.com>
2023-05-16 21:45:53 +0200

    added alert on homepage

--- pages/components/Footer.js
+++ pages/components/Footer.js
@@ -1,31 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 export default function Footer() {
-
-  const [email, setEmail] = useState('');
+  const [email, setEmail] = useState("");
 
   const onSubmit = (e) => {
     e.preventDefault();
     const url =
-      'https://gmail.us21.list-manage.com/subscribe/post-json?u=5dc77ec1e8676c353af843a14&id=77b64696c7&f_id=0053bbe1f0&c=?';
+      "https://gmail.us21.list-manage.com/subscribe/post-json?u=5dc77ec1e8676c353af843a14&id=77b64696c7&f_id=0053bbe1f0&c=?";
     const formData = new FormData();
-    formData.append('EMAIL', email);
+    formData.append("EMAIL", email);
 
     fetch(url, {
-      method: 'POST',
-      mode: 'cors',
+      method: "POST",
+      mode: "cors",
       body: formData,
     })
       .then((response) => response.json())
       .then((data) => {
-        if (data.result === 'success') {
-          alert('Iscrizione avvenuta con successo!');
+        if (data.result === "success") {
+          alert("Iscrizione avvenuta con successo!");
         } else {
-          alert('Si è verificato un errore durante l\'iscrizione.');
+          alert("Si è verificato un errore durante l'iscrizione.");
         }
       })
       .catch((error) => {
-        alert('Si è verificato un errore durante l\'iscrizione.');
+        alert("Si è verificato un errore durante l'iscrizione.");
         console.error(error);
       });
   };
@@ -73,9 +72,9 @@ export default function Footer() {
             <p className="mt-3 max-w-lg text-[15px] text-blue-100 sm:mx-auto">
               No spam, siamo attenti alla protezione dei dati.
             </p>
-            
           </div>
         </div>
+
         <div
           className="absolute inset-0 w-full h-full"
           style={{
@@ -83,6 +82,18 @@ export default function Footer() {
               "linear-gradient(268.24deg, rgba(59, 130, 246, 0.76) 50%, rgba(59, 130, 246, 0.545528) 80.61%, rgba(55, 48, 163, 0) 117.35%)",
           }}
         ></div>
+      </div>
+
+      <div className="mt-8 border-2 rounded-md border-amber-300 relative overflow-hidden mx-4 px-4 py-6 rounded-2xl md:px-8 md:mx-8">
+        <div className="relative z-10 max-w-xl mx-auto sm:text-center">
+          <p className="text-gray-600 leading-relaxed">
+            I contenuti di questo articolo sono puramente informativi. Non
+            sostituiscono in alcun modo il consiglio medico professionale, la
+            diagnosi o il trattamento. Non esitare a consultare un
+            professionista sanitario per qualsiasi domanda riguardante la tua
+            salute o un'eventuale condizione medica.
+          </p>
+        </div>
       </div>
     </section>
   );
